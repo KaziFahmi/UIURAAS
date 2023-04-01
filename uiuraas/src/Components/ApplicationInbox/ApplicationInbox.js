9402ba86d7846883082c4092ea469333fb76dd20
Shahil <shahilyasarhaque@gmail.com>
2023-04-02 03:01:45 +0600

    email inbox front end done

--- uiuraas/src/Components/ApplicationInbox/ApplicationInbox.js
+++ uiuraas/src/Components/ApplicationInbox/ApplicationInbox.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Sidebar from '../SideMenu/SidebarStudent'
+import EmailView from './EmailView'
 function ApplicationInbox() {
   return (
-    <>
-    <Navbar/>
-    <div style={{display: 'flex'}}>
-        <Sidebar/>
-        <div style={{width: '100%'}}>
-            <h1>Email</h1>
-            <div>
-                <div style={{display: 'flex',justifyContent: 'space-between'}}>
-                    <button>Compose</button>
-                    <div style={{display: 'flex'}}>
-                        <p>Inbox</p>
-                        <p>Starred</p>
-                        <p>Snozzed</p>
-                        <p>Sent</p>
-                        <p>Drafts</p>
-                        <p>More</p>
+    <> 
+   
+    <div style={body}  >
+        <h1>Email</h1>
+        <div  >
+            
+            <div style={applicationInbox}>
+                <div style={emailBody} >
+                    <button style={compose}>Compose</button>
+                    <div style={optionSet} >
+                        <p style={options}>Inbox </p>
+                        <br/>
+                        <p style={options}>Starred </p>
+                        <br/>
+                        <p style={options}>Snozzed </p>
+                        <br/>
+                        <p style={options}>Sent </p>
+                        <br/>
+                        <p style={options}>Drafts </p>
+                        <br/>
+                        <p style={options}>More</p>
                     </div>
                 </div>
                 <div>
+                    <EmailView/>
                 </div>
             </div>
         </div>
@@ -30,3 +37,38 @@ function ApplicationInbox() {
   )
 }
 export default ApplicationInbox
+const body={
+    //backgroundColor:"red",
+}
+
+const emailBody={
+    display:"flex",
+    justifyContent:"space-between"
+}
+
+const optionSet={
+    display:"flex",
+    marginRight:".5%"
+    
+}
+const options={
+    borderLeft:"10px solid white",
+    color:" #FC9E04"
+}
+
+const applicationInbox={
+    border:" 1px solid #FC9E04",
+    justifyContent: "flex-start",
+    width: "80vw",
+    height:"80vh"
+  }
+
+  const compose={
+    backgroundColor:"#FC9E04",
+    borderRadius:"15px",
+    border:"none",
+    width:"5%",
+    color:"white",
+    marginTop:".5%",
+    marginLeft:".5%"
+  }
---
+++ uiuraas/src/Components/ApplicationInbox/EmailView.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "../../Styles/application_card.css"
+
+const EmailView = () => {
+  return (
+    <div className='EmailViewCard' style={emailView}>
+        <div className='check'>
+            <input  type="checkbox"/>
+        </div>
+        <div className='subject' style={{}}>
+            Email requesting withdrawal from course
+        </div>
+        
+    </div>
+  )
+}
+
+export default EmailView
+
+const emailView={
+    display:"flex",
+    border:"1px solid #FC9E04",
+    padding:"1%",
+    margin:"2%"
+}
