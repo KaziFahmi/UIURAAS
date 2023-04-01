e6a816a4518fe6c53d4580bcd75daf2cb15631bb
Latin Chakma <lchakma201232@bscse.uiu.ac.bd>
2023-04-01 23:49:31 +0600

    Changed the Inbox Styles

--- uiuraas/src/Components/ApplicationInbox/ApplicationInbox.js
+++ uiuraas/src/Components/ApplicationInbox/ApplicationInbox.js
@@ -7,6 +7,24 @@ function ApplicationInbox() {
     <Navbar/>
     <div style={{display: 'flex'}}>
         <Sidebar/>
+        <div style={{width: '100%'}}>
+            <h1>Email</h1>
+            <div>
+                <div style={{display: 'flex',justifyContent: 'space-between'}}>
+                    <button>Compose</button>
+                    <div style={{display: 'flex'}}>
+                        <p>Inbox</p>
+                        <p>Starred</p>
+                        <p>Snozzed</p>
+                        <p>Sent</p>
+                        <p>Drafts</p>
+                        <p>More</p>
+                    </div>
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
     </div>
     </>
   )
